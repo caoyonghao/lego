776a2f7a92efaff44beaf90ee89f42b24fd4176b
caoyonghao <491476941@qq.com>
2018-06-25 00:35:55 +0800

    update graphql feature

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,37 +2,47 @@ import {
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLString,
+    GraphQLList,
     GraphQLInt
 } from 'graphql'
 
 // 我们要用的模拟数据
 const data = require('../../data/data.json')
 
-const User = new GraphQLObjectType({
-    name: 'User',
-    description: 'User对象',
+const Dashboard = new GraphQLObjectType({
+    name: 'Dashboard',
+    description: 'Dashboard对象',
     fields: {
         id: {
-            type: GraphQLInt
+            type: GraphQLString
         },
-        name: {
+        widgets: {
             type: GraphQLString
         },
     }
 });
 
+const widgetType = new GraphQLObjectType({
+    name: 'Widget',
+    description: 'widget obj',
+    fields: {
+        id: {
+            type: GraphQLString
+        }
+    }
+})
 const Query = new GraphQLObjectType({
     name: 'Query',
     fields: {
-        user: {
-            type: User,
+        widgets: {
+            type: new GraphQLList(new GraphQLList(widgetType)),
             args: {
                 id: {
-                    type: GraphQLInt
+                    type: GraphQLString
                 }
             },
             resolve: function (_, args) {
-                return data[args.id];
+                return data[args.id].widgets;
             }
         }
     }
